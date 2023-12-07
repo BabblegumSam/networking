fdce7e617587d569a424afe6116a669c9e63b455
BabblegumSam <86652018+BabblegumSam@users.noreply.github.com>
2023-12-07 14:32:49 +0000

    yes

---
+++ README.md
@@ -0,0 +1,13 @@
+# Run Node Examples
+ 
+ Before you first run make sure you  have an empty json folder.
+ 
+ In terminal shell run: 
+ 
+ npm install
+ 
+ 
+ Once the packages have installed run:
+ 
+ node app.js
+ 
---
+++ javascript/sketch.js
@@ -0,0 +1,100 @@
+let canvas;
+let adjust;
+
+// Instructions on how to look
+
+// Maybe separate into 3 Classes: PRIESTS, PROFESSIONALS, NPCS
+// Priests = engage the Jester
+// Professionals = active interaction
+// NPCS = repeat same simple actions over and over
+
+// when with someone
+// when alone
+// but always want to
+
+const priestMove = [
+
+];
+
+const priest = [];
+const profs = [];
+
+const npcMove = [
+  "constantly offer a handshake to someone",
+  "look at your phone as often as possible",
+  "pretend to laugh at a joke,"
+
+
+];
+
+
+const npcs = [];
+
+const see = [
+  "avoid all eye contact",
+  "try to make eye contact ",
+  "look at your phone as often as possible",
+  "look at the jester as often as possible",
+  "look at the walls",
+
+];
+
+// Instructions for how to move
+const move = [
+  "move as far away as possible from everyone else ",
+  "move to where the most people are",
+  "move away when someone comes close to you",
+
+];
+
+// Instructions for how to be
+const be = [
+  "copy the actions of the jester as often as possible",
+  "do the opposite of the jester as often as possible",
+  "pray to the jester",
+  "copy the actions of the person furtherst away from you",
+  ""
+
+];
+
+const instruction = [
+  "act like you don't want to be here",
+  "act like you're interested in what someone else is doing",
+  "act like you want to be here but don't know what to do",
+  "make it known to everyone else that you don't want to be here",
+  "pretend that you know what you are doing here",
+  "act like you're important",
+  "act like you want something from someone else"
+];
+
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("sketch-container");
+
+  adjust = windowWidth / 20;
+
+  background(255);
+
+  push();
+  translate(width / 2, height / 2);
+  textWrap(WORD);
+
+  textAlign(CENTER);
+  actionsDisplay();
+  textSize(adjust);
+
+  text("without speaking, " + random(instruction), -windowWidth/4, -windowHeight/4, windowWidth/2);
+
+
+
+
+  pop();
+}
+
+function draw() {
+
+}
+
+function actionsDisplay() {
+
+}
